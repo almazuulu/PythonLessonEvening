76d50f9af2a0177661cbf134932925d91da919dd
almazuulu <almazuulu@gmail.com>
2022-03-10 21:48:01 +0600

    Урок Словари

---
+++ Lesson 6 - Tuple, Set/HW-Tuple.py
@@ -0,0 +1,11 @@
+"""
+Task 2
+
+Создайте кортеж из имен людей.
+Выведите все имена через цикл for по шаблону: "Привет, [ИМЯ]".
+"""
+
+tupleNames = ('Tilek','Aizhan', 'Dinara', 'Yesbol', 'Aselya')
+
+for name in tupleNames:
+    print(f'Привет {name}!')
